a0e4472beb7459dd64f507e157dda219d5d58dc8
Qoreebo1 <shobukolaqoreebadeola@gmail.com>
2025-04-16 11:54:47 +0100

    base

--- hospital/urls.py
+++ hospital/urls.py
@@ -20,5 +20,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home,name="home")
+    path('', views.home,name="home"),
+    path('about/', views.about, name="about"),
+    path('service/', views.service, name="service"),
+    path('contact/', views.contact, name="contact"),
+    path('appointment/', views.appointment, name="contact"),
+
 ]
--- hospital/views.py
+++ hospital/views.py
@@ -3,3 +3,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def appointment(request):
+    return render(request, 'contact.html')
